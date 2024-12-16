e60a95ff725fbeb6d529296d11e0c6562887efb2
Jomarth <117119030+Jomarth@users.noreply.github.com>
2024-12-16 10:50:58 -0600

    Update db.js

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async ()=>{
     try {
-        await mongoose.connect('mongodb://127.0.0.1/cainza');
+        await mongoose.connect(process.env.MONGODB);
         console.log("base de datos conectada");
     } catch (error) {
         console.error(error);
